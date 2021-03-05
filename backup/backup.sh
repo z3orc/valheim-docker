8516ef2a90f442fd1563a872b733851e690fa83a
z3orc <z3orc@users.noreply.github.com>
2021-03-05 22:19:34 +0100

    Added backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cp /home/steam/valheimbackup/mnt/${WORLD}.fwl /home/steam/valheimbackup/dest
-cp /home/steam/valheimbackup/mnt/${WORLD}.db /home/steam/valheimbackup/dest
+cp /home/steam/valheimbackup/mnt/worlds/${WORLD}.fwl /home/steam/valheimbackup/dest
+cp /home/steam/valheimbackup/mnt/worlds/${WORLD}.db /home/steam/valheimbackup/dest
 
 cd /home/steam/valheimbackup/dest
 
---
+++ backup_old/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Start the run once job.
+echo "Docker container has been started"
+
+# Setup a cron schedule
+echo "5 6 * * * home/steam/backup.sh >> /var/log/cron.log 2>&1
+# This extra line makes it a valid cron" > scheduler.txt
+
+crontab scheduler.txt
+cron -f
