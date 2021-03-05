49d4a0f26638772e22955650941c7910a8ea67f9
z3orc <z3orc@users.noreply.github.com>
2021-03-05 16:58:29 +0100

    Added "backup" Dockerfile

---
+++ backup/backup.sh
@@ -0,0 +1,8 @@
+cp /home/steam/valheimbackup/mnt/${WORLD}.fwl /home/steam/valheimbackup/dest
+cp /home/steam/valheimbackup/mnt/${WORLD}.db /home/steam/valheimbackup/dest
+
+cd /home/steam/valheimbackup/dest
+
+tar -cvf ${WORLD}-$(date +"%F@%R").tar ${WORLD}.fwl ${WORLD}.db
+
+rm ${WORLD}.fwl ${WORLD}.db
