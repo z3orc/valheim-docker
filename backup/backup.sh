faf9e2237a116b84293093719435d5e3440193f8
z3orc <z3orc@users.noreply.github.com>
2021-03-07 23:08:24 +0100

    Moved backup to new repo

--- backup/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cp /home/steam/valheimbackup/mnt/worlds/${WORLD}.fwl /home/steam/valheimbackup/dest
-cp /home/steam/valheimbackup/mnt/worlds/${WORLD}.db /home/steam/valheimbackup/dest
-
-cd /home/steam/valheimbackup/dest
-
-tar -cvf ${WORLD}-$(date +"%F@%R").tar ${WORLD}.fwl ${WORLD}.db
-
-rm ${WORLD}.fwl ${WORLD}.db
